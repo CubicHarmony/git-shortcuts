1556c23e7cc116ad33f29bd84bddc30c2ae5ab5e
Nathan Sit <nsit.earth@gmail.com>
2020-03-14 22:09:16 -0700

    added basic aliases in addShortcuts bash script

---
+++ addShortcuts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# These are the shortcuts that I use regularly
+
+# st = status
+git config --global alias.st status
+
+# ci = commit
+git config --global alias.ci commit
+
+# cim = commit -m
+git config --global alias.cim 'commit -m'
