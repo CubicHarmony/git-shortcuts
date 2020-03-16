616a6d7e0318ae5ec9428795a8ef060bdc572716
Nathan Sit <nsit.earth@gmail.com>
2020-03-15 21:14:30 -0700

    Additional basic commands, type, dump, hist not working

--- addShortcuts.sh
+++ addShortcuts.sh
@@ -10,3 +10,11 @@ git config --global alias.ci commit
 
 # cim = commit -m
 git config --global alias.cim 'commit -m'
+
+# co = checkout
+git config --global alias.co checkout
+
+# not sure what these do but they seem useful? .hist needs fixing
+# git config --global alias.hist $'log --pretty=format:'%h %ad | %s%d [%an]' -- graph --date=short'
+git config --global alias.type 'cat-file -t'
+git config --global alias.dump 'cat-file -p'
